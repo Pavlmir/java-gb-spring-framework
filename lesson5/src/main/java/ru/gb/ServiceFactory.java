2929aca32b8d6e87afc2cb856e2e0b1bc11b3883
pavlmir <mirprost@gmail.com>
2021-09-11 09:44:07 +0300

    Урок 5. Java Persistence API. Hibernate. Часть 1

---
+++ lesson5/src/main/java/ru/gb/ServiceFactory.java
@@ -0,0 +1,19 @@
+package ru.gb;
+
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManagerFactory;
+
+@Component // бин для EntityManagerFactory
+public class ServiceFactory {
+    EntityManagerFactory entityManagerFactory = new Configuration()
+            .configure("hibernate.cfg.xml")
+            .buildSessionFactory();
+
+    @Autowired
+    public EntityManagerFactory getEntityManagerFactory(){
+        return entityManagerFactory;
+    }
+}
